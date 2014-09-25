a747ccd8427c20a54feecab5b72e93bc06916cdd
Assaf Alon <assafalon@gmail.com>
2014-09-25 10:23:29 +0300

    first commit

--- GC.py
+++ GC.py
@@ -1,7 +1,33 @@
 __author__ = 'assafalon'
-f = open('GC.fasta','r')
-line = f.readline()
-if line[0] == '>':
-    key = line
+f = open('rosalind_gc.txt','r')
+fasta = {}
+for line in f:
+    if line[0] == ">":
+        key = line[1:].strip()
+        fasta[key] = ""
+    else:
+        fasta[key]+=line.strip()
+f.close()
+for key in fasta:
+    GC=0.0
+    AT=0.0
+    for c in fasta[key]:
+        if c == "G" or c=="C":
+            GC+=1
+        else:
+            AT+=1
+    score = (GC * 100) / (GC+AT)
+    fasta[key] = score
+higest_key = ""
+highest_value = 0.0
+for key in fasta:
+    if fasta[key] > highest_value:
+        higest_key = key
+        highest_value = fasta[key]
+
+print higest_key
+print highest_value
+
+
 
 
---
+++ PERM.py
@@ -0,0 +1,13 @@
+__author__ = 'assafalon'
+#number given
+n = 3
+#calculate factorial
+factorial = n
+while n > 1:
+    n -= 1
+    factorial = factorial * n
+
+
+
+print factorial
+
